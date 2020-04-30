a2b1e3bd49a8555e1d73c16a1fb864bc5e04a4a5
Jiaxun Yang <jiaxun.yang@flygoat.com>
2020-05-01 00:21:00 +0800

    etnaviv: Loongson PCI
    Yes, I fullly understood it's crap, but crap can work!!!!!
    It's ported from my 4.19 tree so using legacy API.
    
    Signed-off-by: Jiaxun Yang <jiaxun.yang@flygoat.com>

--- drivers/gpu/drm/drm_pci.c
+++ drivers/gpu/drm/drm_pci.c
@@ -192,7 +192,7 @@ static void drm_pci_agp_init(struct drm_device *dev)
 	}
 }
 
-static int drm_get_pci_dev(struct pci_dev *pdev,
+int drm_get_pci_dev(struct pci_dev *pdev,
 			   const struct pci_device_id *ent,
 			   struct drm_driver *driver)
 {
@@ -237,6 +237,7 @@ err_free:
 	drm_dev_put(dev);
 	return ret;
 }
+EXPORT_SYMBOL(drm_get_pci_dev);
 
 /**
  * drm_legacy_pci_init - shadow-attach a legacy DRM PCI driver
